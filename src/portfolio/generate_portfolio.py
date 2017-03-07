4ae97da47d112dbe6e6c5028bbb29ac68429b75e
Aaron Colter <aaron.colter@mail.utoronto.ca>
2017-03-06 19:53:39 -0500

    finished simple algorithm to generate portfolios based on risk level user input

--- src/portfolio/generate_portfolio.py
+++ src/portfolio/generate_portfolio.py
@@ -8,85 +8,121 @@ class Portfolio(object):
 
     ''' Initialize empty portfolio, currency maps to current weight in portfolio { currency : value } '''
     portfolio = {}
+
     ''' Risk level of portfolio (set by user) '''
     risk_level = 0
-    '''' USD cash value of portfolio '''
-    value = 0
 
-    ''' Create a new portfolio object '''
-    def __init__(self, risk_level, cash_value):
+    '''' USD cash value of portfolio holdings '''
+    holdings_value = 0
+
+    '''' USD cash value of readily available cash (not all funds will always be invested) '''
+    cash_value = 0
+
+    ''' Create new portfolio object
+        risk_level: integer (1 - 10)
+        cash_value: integer (0]
+     '''
+    def __init__(self, risk_level, portfolio_value):
+
+        # Set class variables, error check
+        if (risk_level < 0 | risk_level > 10):
+            raise ValueError("Risk level must be within 0 - 10")
+        if (portfolio_value < 0):
+            raise ValueError("Portfolio value must be greater than 0")
         self.risk_level = risk_level
+        self.cash_value = portfolio_value
+
+        # Generate portfolio holdings
         self.construct_portfolio()
 
     ''' Construct portfolio considering current market conditions '''
     def construct_portfolio(self):
 
-        #data = self.get_relevant_market_data()
+        # data = self.get_relevant_market_data()
 
         ''' This is where algorithm will be implemented to create a portfolio.
             For first iteration algorithm will be simple,
             if user risk level is < 5
-                construct equally weighted (10 units each) portfolio of: Litecoin, NEM, MaidSafeCoin, Augur, Zcash
+                construct portfolio of: Bitcoin, Ethereum, Dash, Ripple, Monero (with varying weights)
             if risk level >= 5
-                construct equally weighted (10 units each) portfolio of: Bitcoin, Ethereum, Dash, Ripple, Monero
+                construct portfolio of: Litecoin, NEM, MaidSafeCoin, Augur, Zcash (with varying weights)
         '''
         if self.risk_level < 5:
-            for currency in range(0, 5):
-                self.portfolio[currencies[currency]] = 10
-        else:
-            for currency in range(5, 10):
-                self.portfolio[currencies[currency]] = 10
+            self.portfolio['Bitcoin'] = 4
+            self.portfolio['Ethereum'] = 5
+            self.portfolio['Dash'] = 10
+            self.portfolio['Ripple'] = 10
+            self.portfolio['Monero'] = 5
+            invested_value = ((4 * self.get_price('Bitcoin') + (5 * self.get_price('Ethereum')) + (10 * self.get_price('Dash')) +
+                                                 (10 * self.get_price('Ripple')) + (5 * self.get_price('Monero'))))
+            self.holdings_value = invested_value
+            self.cash_value -= invested_value
 
-    ''' Construct portfolio considering current market conditions '''
-    def rebalance_portfolio(self):
+        else:
+            self.portfolio['Bitcoin'] = 7
+            self.portfolio['Litecoin'] = 137
+            self.portfolio['NEM'] = 13
+            self.portfolio['MaidSafeCoin'] = 25
+            self.portfolio['Augur'] = 10
+            self.portfolio['Zcash'] = 12
+            invested_value = ((7 * self.get_price('Bitcoin')) + (137 * self.get_price('Litecoin') +
+                                (13 * self.get_price('NEM')) + (25 * self.get_price('MaidSafeCoin'))
+                                + (10 * self.get_price('Augur')) + (12 * self.get_price('Zcash'))))
+            self.holdings_value = invested_value
+            self.cash_value -= invested_value
+
+        return
+
+    ''' Re-balance portfolio considering current market conditions '''
+    def balance_portfolio(self):
         #TODO (next iteration)
         return
 
 
-    ''' Calculate current value of portfolio based on current crypto-currency market and holdings '''
-    def calculate_portfolio_value(self):
+    def get_total_portfolio_value(self):
 
-        # Use market object to obtain latest market data
-        market = market_data.Market()
-
-        value = 0
-        # loop through elements in portfolio, get current price
-        for currency in self.portfolio:
-            crypto_price = float(json.loads(market.ticker(currency))[0].get('price_usd'))
-            value += (crypto_price * self.portfolio[currency])
-
-        self.value = value
-
+        return self.cash_value + self.holdings_value
 
 
     ''' Send portfolio to front end for display '''
     def send_portfolio(self):
+        #TODO
         return
 
 
-    ''' Return json object of crypto-currencies to consider for portfolio
+    ''' Helper function that returns json object of crypto-currencies to consider for portfolio
         used in construction + re-balancing of portfolio object
     '''
-    def get_relevant_market_data(self):
+    def get_relevant_market_data(self, start, end):
 
         # Create new market object to get information
         market = market_data.Market()
 
         # Build json object of relevant currency data
         data = {}
-        for currency in currencies:
-            data[currency] = market.ticker(currency)
+        for currency in range(start, end):
+            data[currencies[currency]] = market.ticker(currencies[currency])
         json_data = json.dumps(data)
         return data
 
+    ''' Helper function to get current price of currency '''
+    def get_price(self, currency):
+
+        # Use market object to obtain latest market data
+        market = market_data.Market()
+
+        crypto_price = float(json.loads(market.ticker(currency))[0].get('price_usd'))
+        return crypto_price
 
 
 if __name__ == '__main__':
 
     print " running "
 
-    risk_level = 6
-    portfolio = Portfolio(risk_level)
+    risk_level = 3
+    portfolio_value = 10000
+    portfolio = Portfolio(risk_level, portfolio_value)
     print portfolio.portfolio
-    portfolio.calculate_portfolio_value()
-    print portfolio.value
+    print portfolio.holdings_value
+    print portfolio.cash_value
+    # print portfolio.get_total_portfolio_value()2
