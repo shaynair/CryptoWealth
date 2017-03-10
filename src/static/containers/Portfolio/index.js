b29d467f4d22a89d2ce7d880d7d79fdf8e4b3e3e
sakshaat_dc <sakshaat_dc@hotmail.com>
2017-03-09 22:13:43 -0500

    HI

--- src/static/containers/Portfolio/index.js
+++ src/static/containers/Portfolio/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 // import { Link } from 'react-router';
 import { connect } from 'react-redux';
+import { PortfolioTable } from './PortfolioTable';
+
+var portfolioA = {
+    data: [
+        {
+            symbol: "BTC",
+            name: "Bitcoin",
+            alloc: 20
+        },
+        {
+            symbol: "ETC",
+            name: "Ethereum",
+            alloc: 21
+        },
+        {
+            symbol: "DASH",
+            name: "Dash",
+            alloc: 19
+        }
+    ]
+}
+
 
 class PortfolioView extends React.Component {
   render() {
-    return (<h1> hi </h1>);
+    return (<PortfolioTable portfolio={ portfolioA } > </PortfolioTable>);
   }
 }
 
