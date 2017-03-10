c95e2921781b71e37e1b7512fe689fee7d8a55c2
sakshaat_dc <sakshaat_dc@hotmail.com>
2017-03-10 00:05:43 -0500

    Added some more styling

--- src/static/containers/Portfolio/index.js
+++ src/static/containers/Portfolio/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { dataFetchProtectedData } from '../../actions/portfolio';
-import { PortfolioTable } from './PortfolioTable';
+import { PortfolioTable } from '../../components/PortfolioTable';
 
 
 class PortfolioView extends React.Component {
