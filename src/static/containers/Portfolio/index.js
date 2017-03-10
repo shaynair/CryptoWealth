6f78abcfab78fee05569d4427210d87c52764cf7
Aaron Colter <aaron.colter@mail.utoronto.ca>
2017-03-09 21:48:59 -0500

    finished backend/front end connection

--- src/static/containers/Portfolio/index.js
+++ src/static/containers/Portfolio/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 // import { Link } from 'react-router';
 import { connect } from 'react-redux';
+import { dataFetchProtectedData } from '../../actions/portfolio';
 
 class PortfolioView extends React.Component {
+
+  static propTypes = {
+      dispatch: React.PropTypes.func.isRequired
+  };
+
+  componentWillMount() {
+    this.props.dispatch(dataFetchProtectedData(0, 10));
+  }
+
   render() {
-    return (<h1> hi </h1>);
+    return (<p> {JSON.stringify(this.props.data)} </p>);
   }
 }
 
 const mapStateToProps = (state) => {
     return {
+      data: state.portfolio.data
     };
 };
 
--- src/static/reducers/portfolio.js
+++ src/static/reducers/portfolio.js
@@ -1,5 +1,5 @@
 import { createReducer } from '../utils';
-import { DATA_RECEIVE_PROTECTED_DATA, DATA_FETCH_PROTECTED_DATA_REQUEST } from '../constants';
+import { DATA_RECEIVE_PORTFOLIO, DATA_FETCH_PORTFOLIO_REQUEST } from '../constants';
 
 const initialState = {
     data: null,
@@ -7,13 +7,13 @@ const initialState = {
 };
 
 export default createReducer(initialState, {
-    [DATA_RECEIVE_PROTECTED_DATA]: (state, payload) => {
+    [DATA_RECEIVE_PORTFOLIO]: (state, payload) => {
         return Object.assign({}, state, {
             data: payload.data,
             isFetching: false
         });
     },
-    [DATA_FETCH_PROTECTED_DATA_REQUEST]: (state, payload) => {
+    [DATA_FETCH_PORTFOLIO_REQUEST]: (state, payload) => {
         return Object.assign({}, state, {
             isFetching: true
         });
