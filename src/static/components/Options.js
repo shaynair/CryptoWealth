a40ef9565bae3cd6b495a4ad508755c756798019
Ian Hibbert <ian.hibbert.a@gmail.com>
2017-03-22 01:33:23 -0400

    Fixed reducers being called twice on every action going back on the questionnaire updates risk

--- src/static/components/Options.js
+++ src/static/components/Options.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Options extends Component {
-  static propTypes = {
-    propText: React.PropTypes.string.isRequired,
-    name: React.PropTypes.number.isRequired
-  };
-  render() {
-    return (
-      <li className="col-md-8">
-        <input type="radio" name={this.props.name} />
-        <span className="option-text"> {this.props.propText} </span>
-      </li>
-    );
-  }
+    static propTypes = {
+        text: React.PropTypes.string.isRequired,
+        name: React.PropTypes.number.isRequired,
+        answer: React.PropTypes.number.isRequired,
+    };
+
+    render() {
+        if (this.props)
+        return (
+            <li className="col-md-8">
+                <input type="radio" name={this.props.name}/>
+                <span className="option-text"> {this.props.text} </span>
+            </li>
+        );
+    }
 }
 
 export default Options;
