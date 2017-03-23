c257918fd1e26700d3e9cf2567aba67aee20a0e9
Aaron Colter <aaron.colter@mail.utoronto.ca>
2017-03-23 00:18:28 -0400

    working on home page

--- src/static/containers/Home/index.js
+++ src/static/containers/Home/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
+import BubbleExample from './chart';
 
-import './style.scss';
 import CryptoWealthLogo from './images/CryptoWealthLogo.png';
 
 class HomeView extends React.Component {
@@ -13,29 +13,59 @@ class HomeView extends React.Component {
   };
 
   render() {
-    return (
-        <div className="container">
-            <div className="margin-top-medium text-center">
-                <img className="page-logo margin-bottom-medium"
-                    src={CryptoWealthLogo}
-                    alt="ReactJs"
-                />
-            </div>
-            <div className="text-center">
-              <h1 > CryptoWealth < /h1> <h4 > Hello, { this.props.userName || 'guest' }. < /h4>
-            </div> <div className="margin-top-medium"> {
-                this.props.statusText ? <div className="alert alert-info"> { this.props.statusText }
-                </div> :null
-            }
 
-            <div>
-              <h1> If you don't want to be poor forever then click the button below!! </h1>
+    const styles = {
+      containerStyle: {
+        width: '100%',
+        height: '100%',
+      },
+      helloStyle: {
+        paddingLeft: '270px'
+      },
+      titleStyle: {
+        paddingLeft: '210px'
+      },
+      descriptionStyle: {
+        paddingTop: '100px'
+      },
+      imageStyle: {
+        width: 600,
+        height: 550
+      }
+    };
+
+    return (
+      <div className="container" style={styles.containerStyle}>
+          <div className="row">
+
+            <div className="col-md-6">
+              <div className="row">
+                <div className="imageContainer">
+                  <img style={styles.imageStyle} src={CryptoWealthLogo} alt="ReactJs" />
+                </div>
+              </div>
+              <div className="row">
+                <div>
+                  <h1 style={styles.titleStyle}> CryptoWealth < /h1> <h4 style={styles.helloStyle} > Hello, { this.props.userName || 'guest' }. < /h4>
+                </div>
+              </div>
             </div>
+
+            <div className="col-md-6">
+              <row>
+                <BubbleExample />
+              </row>
+              <div>
+                <h1 style={styles.descriptionStyle}> If you dont want to be poor forever then click the button below!! </h1>
+              </div>
+
+              <div>
+                <Link to="/questionnaire">Start Questionnaire </Link>
+              </div>
             </div>
-            <div className="row">
-              <Link className="btn btn-default col-md-4 col-md-offset-4" to="/questionnaire">Start Questionnaire </Link>
-            </div>
+
         </div>
+      </div>
     );
   }
 }
