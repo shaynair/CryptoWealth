6aa2e01ee03340fc0c3357cb844b8d7e484ed1aa
sakshaat_dc <sakshaat_dc@hotmail.com>
2017-03-23 18:27:08 -0400

    Changes to profile

--- src/static/routes.js
+++ src/static/routes.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './app';
-import { HomeView, LoginView, ProtectedView, QuestionnaireView, NotFoundView, PortfolioView , ProfileView} from './containers';
+import { HomeView, LoginView, ProtectedView, QuestionnaireView, NotFoundView, PortfolioView , ProfileView, AnalyticsView } from './containers';
 import requireAuthentication from './utils/requireAuthentication';
 
 export default(
   <Route path="/" component={App}>
     <IndexRoute component={ requireAuthentication(ProfileView, HomeView)} />
-    <Route path="login" component={LoginView} />
-    <Route path="protected" component={requireAuthentication(ProtectedView)} />
+    <Route path="login" component={LoginView} /> />
     <Route path="questionnaire" component={QuestionnaireView} />
     <Route path="portfolio" component={PortfolioView} />
     <Route path="profile" component={requireAuthentication(ProfileView)} />
+    <Route path="analytics" component={requireAuthentication(AnalyticsView, HomeView)} />
     <Route path="*" component={NotFoundView} />
   </Route>
 );
