878880a96a2b7fd318970b6ca597f9e452fd0cec
Akshay Nair <hello@akshaynair.com>
2017-04-02 23:38:04 -0400

    Add an endpoint

--- src/portfolio/urls.py
+++ src/portfolio/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(_(r'^risk/$'), BasicRiskView.as_view(), name='risk'),
     url(_(r'^portfolio/$'), UserPortfolioView.as_view(), name='portfolio'),
     url(_(r'^historical/$'), UserHistoricalView.as_view(), name='historical'),
+    url(_(r'^activity/$'), UserActivityView.as_view(), name='activity'),
     url(_(r'^currency/(?P<symbol>\w+)$'), CurrencyView.as_view(), name='currency'),
 ]
