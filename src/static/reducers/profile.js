dcff13ef6a4d142721c1b7c285e0708cdec2aeeb
Akshay Nair <hello@akshaynair.com>
2017-04-03 16:57:53 -0400

    Improved activity log

--- src/static/reducers/profile.js
+++ src/static/reducers/profile.js
@@ -35,12 +35,12 @@ export default createReducer(initialState, {
         return Object.assign({}, state, {
             activityLog: payload.activityLog
         })
+    },
     [HISTORY_FETCH_REQUEST]: (state) => {
         return Object.assign({}, state, {
         });
     },
     [HISTORY_FETCH_SUCCESS]: (state, payload) => {
-        console.log(payload);
         return Object.assign({}, state, {
             history: payload,
         });
