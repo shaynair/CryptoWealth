9fd2ac95d8c72eb859dd2b89e146d2f01ec1e49b
sakshaat_dc <sakshaat_dc@hotmail.com>
2017-04-02 23:45:58 -0400

    Did trend grap

--- src/static/reducers/profile.js
+++ src/static/reducers/profile.js
@@ -1,12 +1,13 @@
 import { createReducer } from '../utils';
-import { PROFILE_FETCH_FAILURE, PROFILE_FETCH_REQUEST, PROFILE_FETCH_SUCCESS } from '../constants';
+import { PROFILE_FETCH_FAILURE, PROFILE_FETCH_REQUEST, PROFILE_FETCH_SUCCESS, HISTORY_FETCH_FAILURE, HISTORY_FETCH_REQUEST, HISTORY_FETCH_SUCCESS } from '../constants';
 
 const initialState = {
     portfolios: [],
     isFetching: false,
     returns: 0,
     totalEarnings: 0,
-    totalValue: 0
+    totalValue: 0,
+    history: {}
 };
 
 export default createReducer(initialState, {
@@ -16,7 +17,6 @@ export default createReducer(initialState, {
         });
     },
     [PROFILE_FETCH_SUCCESS]: (state, payload) => {
-        console.log(payload);
         return Object.assign({}, state, {
             portfolios: payload.portfolio,
             isFetching: true,
@@ -28,6 +28,20 @@ export default createReducer(initialState, {
     [PROFILE_FETCH_FAILURE]: (state) => {
         return Object.assign({}, state, {
             isFetching: false,
+        });
+    },
+    [HISTORY_FETCH_REQUEST]: (state) => {
+        return Object.assign({}, state, {
+        });
+    },
+    [HISTORY_FETCH_SUCCESS]: (state, payload) => {
+        console.log(payload);
+        return Object.assign({}, state, {
+            history: payload,
+        });
+    },
+    [HISTORY_FETCH_FAILURE]: (state) => {
+        return Object.assign({}, state, {
         });
     }
 });
